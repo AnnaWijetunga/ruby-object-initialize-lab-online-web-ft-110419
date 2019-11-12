7b9c2fa0cd4335907efcc81ae83ddc7c9befbc9a
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-12 02:31:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,27 @@
-class Dog 
-  def initialize(name, breed)
-    @name = name
-    @breed = breed 
-  end
+# class Dog 
+#   def initialize(name, breed)
+#     @name = name
+#     @breed = breed 
+#   end
   
-  def breed=(breed)
+#   def breed=(breed)
+#     @breed = breed
+#   end
+  
+#   def breed
+#     @breed
+#   end
+  
+# # dog.breed = "Mutt"
+
+# end
+
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
     @breed = breed
   end
-  
-  def breed
-    @breed
-  end
-  
-# dog.breed = "Mutt"
-
 end
+fido = Dog.new("Fido")
 
 
